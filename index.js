1894bf179e11818a3fa23a8c5473e9bde8d899da
akshita-2505 <akshitapatel522@gmail.com>
2019-05-15 12:29:34 +0530

    web add

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+//  react-native app run in web
+//  https://github.com/joefazz/react-native-web-starter
+// 1. npm install
+// 2. npm run web
+
+
 import 'core-js';
 import {AppRegistry} from 'react-native';
 import App from './mapRoad';
